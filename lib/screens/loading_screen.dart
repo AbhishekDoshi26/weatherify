106b9240d03f7a78e5c304789235ba299360bc84
AbhishekDoshi26 <adoshi26.ad@gmail.com>
2020-05-12 22:31:07 +0530

    Update loading_screen.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:weatherify/services/location.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -24,9 +25,13 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getData() async {
     http.Response response = await http.get(
         'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
-    if (response.statusCode == 200)
+    if (response.statusCode == 200) {
       String data = response.body;
-    else
+      var decodedData = jsonDecode(data);
+      var temperature = decodedData['main']['temp'];
+      var condition = decodedData['weather'][0]['id'];
+      var cityName = decodedData['name'];
+    } else
       print(response.body);
   }
 
